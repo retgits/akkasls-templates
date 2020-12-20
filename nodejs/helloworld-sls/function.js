17ccdb566ba376bde6a9da154839f48b19945bc8
retgits <lstigter@gmail.com>
2020-12-19 17:13:40 -0800

    feat: add sls project

---
+++ nodejs/helloworld-sls/function.js
@@ -0,0 +1,43 @@
+const crdt = require("cloudstate").crdt;
+
+const entity = new crdt.Crdt(
+    "userfunction.proto",
+    "{{protopackage}}.HelloWorld",
+    {}
+);
+
+entity.commandHandlers = {
+    Increment: increment,
+    Get: get
+};
+
+function increment(update, ctx) {
+    if (update.value < 0) {
+        ctx.fail("Cannot decrement gcounter");
+    }
+
+    if (ctx.state === null) {
+        ctx.state = new crdt.GCounter();
+    }
+
+    if (update.value > 0) {
+        ctx.state.increment(update.value);
+    }
+
+    return {
+        value: ctx.state.value
+    };
+}
+
+function get(get, ctx) {
+    if (ctx.state === null) {
+        ctx.state = new crdt.GCounter();
+    }
+
+    return {
+        value: ctx.state.value
+    };
+}
+
+// Export the entity
+module.exports = entity;
